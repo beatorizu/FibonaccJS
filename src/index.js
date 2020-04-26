a0f65fe8b9f28d7e0275a13e67ffa056dd8f9ff4
beatorizu <betrizaf@gmail.com>
2020-04-26 13:20:45 -0300

    feat: Generate fibonacci sequence

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 'use strict'
 
 const fibonacci = () => {
-    return []
+    let fibonacciSequence = [];
+    let j = 1;
+    let i = 0;
+
+    for (let k = 0; k < 14; k++) {
+        let t = i + j;
+        i = j;
+        j = t;
+        fibonacciSequence.push(t);
+    }
+    return fibonacciSequence;
 }
 
 const isFibonnaci = (num) => fibonacci().includes(num)
